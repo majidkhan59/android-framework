8aa9d7be5ad34a901e8ea20316ca146d4a1b109f
Shoaib Ahmed <shoaibzzz@hotmail.com>
2018-04-24 23:37:38 +1000

    added toolbar

--- Android Framework/src/android/framework/AndroidFramework.java
+++ Android Framework/src/android/framework/AndroidFramework.java
@@ -41,25 +41,13 @@ public class AndroidFramework extends JPanel {
         //Add the SatellitView to the scene:
         add(scene.createSatelliteView(), BorderLayout.WEST);
         
-        JToolBar toolbar = new JToolBar("Controls", JToolBar.VERTICAL);
-        toolbar.setLayout(new FlowLayout());
-        toolbar.setBackground(Color.white);
-        toolbar.setPreferredSize(new Dimension(150,150));
+        Toolbar toolbar = new Toolbar("Controls", JToolBar.VERTICAL);
+         
+        toolbar.addButton("Build APK", "Generate APK", 100, 100);
+        toolbar.addButton("Add Button", "Add new button", 100, 100);
+        toolbar.addButton("Add Label", "Add new label", 100, 100);
+        toolbar.addButton("Remove", "Remove the control", 100, 100);
         
-        JButton button = new JButton("Build APK");
-        button.setPreferredSize(new Dimension(100, 100));
-        button.setToolTipText("Generate APK");
-        toolbar.add(button);
-        
-        JButton button2 = new JButton("Add Button");
-        button2.setPreferredSize(new Dimension(100, 100));
-        toolbar.add(button2);
-        
-        JButton button3 = new JButton("Add Label");
-        button3.setPreferredSize(new Dimension(100, 100));
-        toolbar.add(button3);
-        
-        toolbar.setFloatable(false);
      //   toolbar.addSeparator();
         add(toolbar, BorderLayout.EAST);
        
---
+++ Android Framework/src/android/framework/Toolbar.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package android.framework;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.util.ArrayList;
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+
+/**
+ * This class manages toolbar of the application.
+ * 
+ * @author shoaibahmed
+ */
+public class Toolbar extends JToolBar{
+    
+    private ArrayList<JButton> buttons = new ArrayList<>();
+  
+    /**
+     * This constructor generates a toolbar with given information.
+     * 
+     * @param name The name of the toolbar.
+     * @param orientation The default orientation of the toolbar.
+     */
+    public Toolbar(String name, int orientation) {
+        super(name, orientation);
+        this.setLayout(new FlowLayout());
+        this.setBackground(Color.white);
+        this.setPreferredSize(new Dimension(150,150));
+        this.setFloatable(false);
+    }
+    
+    /**
+     * This function adds a button with specified information in the toolbar.
+     * 
+     * @param text The text of the button to be displayed.
+     * @param tooltip The tooltip text of the button to be displayed.
+     * @param height The height of the button.
+     * @param width The width of the button.
+     */
+    public void addButton(String text, String tooltip, int width, int height){
+        JButton buttonToAdd = new JButton(text);
+        buttonToAdd.setToolTipText(tooltip);
+        buttonToAdd.setPreferredSize(new Dimension(width, height));
+        buttons.add(buttonToAdd);
+        add(buttonToAdd);
+    }
+    
+    
+    
+}
